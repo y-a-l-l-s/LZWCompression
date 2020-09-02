9d819e0c084a937358bfae6f9bf7efac29377be9
Lukas Seklir <lukas.seklir@gmail.com>
2020-09-02 12:53:15 -0700

    first 256 characters are added to the hashmap

--- src/LZWCodeGenerator.java
+++ src/LZWCodeGenerator.java
@@ -3,9 +3,17 @@ import java.awt.List;
 
 public class LZWCodeGenerator {
 
-	public static Map compressText(String originalText) {
-		Map<String, Integer> dict = new HashMap<String, Integer>();
-		return dict;
+	public static ArrayList compressText(String originalText) {
+		HashMap<String, Integer> dict = new HashMap<String, Integer>();
+		int dictSize = 2048;
+		ArrayList<Integer> compressedText = new ArrayList<Integer>();
+		
+		for (int i = 0; i < 256; i++) {
+			dict.put("" + (char)i, i);
+		}
+		
+		
+		
+		return compressedText;
 	}
-
 }
