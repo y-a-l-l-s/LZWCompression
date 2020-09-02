0aa58a0f667f5c78eb5c23c076e53e1525afc96b
Lukas Seklir <lukas.seklir@gmail.com>
2020-09-02 12:46:46 -0700

    added compressor function

--- src/LZWCodeGenerator.java
+++ src/LZWCodeGenerator.java
@@ -1,4 +1,11 @@
+import java.util.*;
+import java.awt.List;
 
 public class LZWCodeGenerator {
+
+	public static Map compressText(String originalText) {
+		Map<String, Integer> dict = new HashMap<String, Integer>();
+		return dict;
+	}
 
 }
