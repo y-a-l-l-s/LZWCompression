6064d3bfd7593d0fb395bb11a456caac6f8a6e77
Lukas Seklir <lukas.seklir@gmail.com>
2020-09-02 13:02:54 -0700

    iterate through string and add to hashmap

--- src/LZWCodeGenerator.java
+++ src/LZWCodeGenerator.java
@@ -5,13 +5,28 @@ public class LZWCodeGenerator {
 
 	public static ArrayList compressText(String originalText) {
 		HashMap<String, Integer> dict = new HashMap<String, Integer>();
-		int dictSize = 2048;
+		int dictSize = 256;
+		int maxSize = 2048;
 		ArrayList<Integer> compressedText = new ArrayList<Integer>();
 		
 		for (int i = 0; i < 256; i++) {
 			dict.put("" + (char)i, i);
 		}
 		
+		String old = "";
+		for (char character: originalText.toCharArray()) {
+			String current = old + character;
+			
+			if (dict.containsKey(current)) {
+				old = current;
+			}
+			// have to also add something here to make sure that it doesn't exceed the max size
+			else {
+				dict.put(current, dictSize++);
+				compressedText.add(dict.get(old));
+				old = Character.toString(character);
+			}
+		}
 		
 		
 		return compressedText;
