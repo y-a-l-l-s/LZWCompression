f94a44e2c2268fc8d80eace1266f07bb99a25ee6
Lukas Seklir <lukas.seklir@gmail.com>
2020-09-04 10:18:17 -0700

    changed method to void

--- src/LZWCodeGenerator.java
+++ src/LZWCodeGenerator.java
@@ -1,17 +1,19 @@
 import java.util.*;
 import java.awt.List;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class LZWCodeGenerator {
-
+	
 	public static void main(String [] args) {
 		
 	}
 
-	public static ArrayList compressText(String inputFile) throws IOException {
+	public static void compressText(String inputFile) throws IOException {
 		// all starting characters
 		int dictSize = 256;
 		// max length for the hashmap
@@ -19,6 +21,8 @@ public class LZWCodeGenerator {
 		
 		HashMap<String, Integer> dict = new HashMap<String, Integer>();
 		ArrayList<Integer> compressedText = new ArrayList<Integer>();
+		
+		StringBuffer encodedText = new StringBuffer();
 		
 		// adds all 256 characters to the hashmap
 		for (int i = 0; i < 256; i++) {
@@ -47,6 +51,6 @@ public class LZWCodeGenerator {
 		}
 		
 		// returns the arraylist to be converted to .txt file
-		return compressedText;
+		//return compressedText;
 	}
 }
