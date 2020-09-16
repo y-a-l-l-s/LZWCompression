618d42712789e534a1b1dbee1bd65bb20a158419
isarodriguez9 <70167519+isarodriguez9@users.noreply.github.com>
2020-09-16 13:23:56 -0700

    fixing encoder

--- src/LZWCodeGenerator.java
+++ src/LZWCodeGenerator.java
@@ -7,22 +7,20 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class LZWCodeGenerator {
-final int DICTINT = 265;
+final static int DICTINT = 128;
+final static int NUMOFBINDIGITS = 12;
+final static int CHARDIGITS = 16;
+final int MAXHASHSIZE = (int) Math.pow(2,  NUMOFBINDIGITS);
 	public static void main(String [] args) throws IOException {
 		compressText("fileToCompress.txt");
 	}
 
 	public static void compressText(String inputFile) throws IOException {
-		// all starting characters
-		int dictSize = 256;
-		// max size for the hashmap
-		int maxSize = 4096;
-		
 		HashMap<String, Integer> dict = new HashMap<String, Integer>();
 		ArrayList<Integer> compressedText = new ArrayList<Integer>();
 		
 		// adds all 256 characters to the hashmap
-		for (int i = 0; i < 256; i++) {
+		for (int i = 0; i < DICTINT; i++) {
 			dict.put("" + (char)i, i);
 		}
 		
@@ -55,24 +53,24 @@ final int DICTINT = 265;
 		for (int i = 0; i < compressedText.size(); i++) { // convert array list to binary and add to string
 			String num = Integer.toBinaryString(compressedText.get(i)); 
 			// pads 0s to make it 8 long
-			while(num.length() < 8) {
+			while(num.length() < NUMOFBINDIGITS) {
 				num = "0" + num;
 			}
 			str.append(num);
 			
-			while(str.length() > 8) { // convert binary to encoded character and add to .txt
-				pw.print((char)Integer.parseInt(str.substring(0,8), 2));
-				str.delete(0, 8);
+			while(str.length() > CHARDIGITS) { // convert binary to encoded character and add to .txt
+				pw.print((char)Integer.parseInt(str.substring(0,CHARDIGITS), 2));
+				str.delete(0, CHARDIGITS);
 			}
 		}
-		int left = 8 - (str.length() % 8); // add extra zeroes for padding
-		if(left != 8) {
+		int left = CHARDIGITS - (str.length() % CHARDIGITS); // add extra zeroes for padding
+		if(left != CHARDIGITS) {
 			for(int j = 0; j < left; j++) {
 				str.append("0");
 			}
 		}
-		for(int i = 0; i < str.length(); i = i + 8) { // add padded ending to .txt
-			pw.print((char)Integer.parseInt(str.substring(i,i+8), 2));
+		for(int i = 0; i < str.length(); i = i + CHARDIGITS) { // add padded ending to .txt
+			pw.print((char)Integer.parseInt(str.substring(i,i+CHARDIGITS), 2));
 		}
 		pw.print((char)left); // add character for number of extra zeroes
 		pw.close();
