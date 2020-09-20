8a04ee93a160a440d95eec6eceb1b371e2f757b2
isarodriguez9 <70167519+isarodriguez9@users.noreply.github.com>
2020-09-20 16:57:32 -0700

    change

--- src/LZWCodeGenerator.java
+++ src/LZWCodeGenerator.java
@@ -1,7 +1,5 @@
 import java.util.*;
-import java.awt.List;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
